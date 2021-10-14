460b312604d6e596e65e32771a376102d3584dff
Evan Li <eli24@ucsc.edu>
2021-10-14 10:12:58 -0700

    background, rock gravity

--- docs/Extinction/main.js
+++ docs/Extinction/main.js
@@ -19,12 +19,12 @@ const G = {
     WIDTH: 600,
     HEIGHT: 600,
 
-    GRAVITY: 2,
+    GRAVITY: 0.2,
 };
 
 options = {
     viewSize: {x: G.WIDTH, y: G.HEIGHT},
-    theme: "shapeDark",
+    theme: "simple",
     isReplayEnabled: true,
     // isPlayingBgm: true,
     seed: 3,
@@ -36,8 +36,10 @@ options = {
 * @property { Color } color
 * @property { Vector } pos
 * @property { Vector } velocity
-* @property { boolean } Enablegravity
+* @property { Boolean } enableGravity
+* @property { Number } size
 * @property { Number } decel
+* @property { Number } offScreenDist
 * @property { Number } stopSpeed
 * @property { Number } throwCooldown
 * @property { Number } throwCooldownCount
@@ -48,6 +50,7 @@ options = {
 * @property { Color } color
 * @property { Vector } pos
 * @property { Vector } velocity
+* @property { Number } size
 * @property { Number } decel
 * @property { Number } speed
 * @property { Number } throwSpeed
@@ -64,11 +67,16 @@ let player;
 */
 let rocks, dinos;
 
+let gravityEnableHeight = G.HEIGHT * 0.3
+let groundHeight = G.HEIGHT * 0.7
+
 function update() {
 
     if (!ticks) {
         Start();
     }
+
+    RenderBackground();
 
     RenderPlayer();
 
@@ -85,6 +93,7 @@ function Start()
         color: "cyan",
         pos: vec(G.WIDTH * 0.5, G.HEIGHT * 0.5),
         velocity: vec(0, 0),
+        size: 20,
         decel: 0.9,
         speed: 0.1,
         throwSpeed: 3,
@@ -92,7 +101,7 @@ function Start()
     }
 
     rocks = [];
-    times(10, () => {SpawnRock()});
+    times(3, () => {SpawnRock()});
     dinos = [];
     times(10, () => {SpawnDino()});
 }
@@ -113,19 +122,34 @@ function RenderPlayer()
     player.velocity = newVelocity;
     player.velocity = DecelVector(player.velocity, player.decel);
 
-    player.pos.clamp(0, G.WIDTH, 0, G.HEIGHT);
+    player.pos.clamp(player.size/2, G.WIDTH - player.size/2,
+        player.size/2, gravityEnableHeight - player.size/2);
 
     color(player.color);
-    box(player.pos.x, player.pos.y, 20);
+    box(player.pos.x, player.pos.y, player.size);
+}
+
+function RenderBackground()
+{
+    color("black");
+    rect(0, 0, G.WIDTH, gravityEnableHeight);
+    color("light_cyan");
+    rect(0, gravityEnableHeight, G.WIDTH, G.HEIGHT);
+
+    color("light_green");
+    rect(0, groundHeight, G.WIDTH, G.HEIGHT);
 }
 
 function SpawnRock() {
     rocks.push({
-        color: "black",
-        pos: vec(0, rnd((G.HEIGHT * 0.15), (G.HEIGHT * .5))),
-        velocity: vec(rnd(3,6), 0),
+        color: "light_black",
+        pos: vec(rndi(-10, 10),
+            rnd(G.HEIGHT * 0.1,gravityEnableHeight - G.HEIGHT * 0.1)),
+        velocity: vec(rnd(0.5,2), 0),
         decel: 0.95,
-        Enablegravity: false,
+        enableGravity: false,
+        size: 10,
+        offScreenDist: 20,
         stopSpeed: 1,
         throwCooldown: 60,
         throwCooldownCount: 0,
@@ -139,7 +163,7 @@ function SpawnDino(){
         pos: vec(0, rnd((G.HEIGHT * 0.15), (G.HEIGHT * .5))),
         velocity: vec(rnd(3,6), 0),
         decel: 0.95,
-        Enablegravity: false,
+        enableGravity: false,
         stopSpeed: 1,
         throwCooldown: 60,
         throwCooldownCount: 0,
@@ -152,19 +176,18 @@ function RenderRocks()
         // let slowDownVector = DecelVector(rock.velocity, rock.decel);
         // rock.velocity = slowDownVector;
 
-        if(rock.velocity.length <= rock.stopSpeed)
+        if(rock.enableGravity && rock.pos.y > gravityEnableHeight)
         {
-            rock.velocity = vec(0, 0);
-        }
-        else
-        {
-            rock.pos.add(rock.velocity);
+            rock.velocity.add(vec(0, G.GRAVITY));
         }
 
-        rock.pos.wrap(0, G.WIDTH, 0, G.HEIGHT);
+        rock.pos.add(rock.velocity);
+
+        rock.pos.wrap(-rock.offScreenDist, rock.offScreenDist + G.WIDTH,
+            -rock.offScreenDist, rock.offScreenDist + G.HEIGHT);
 
         color(rock.color);
-        let isOnPlayer = box(rock.pos.x, rock.pos.y, 10)
+        let isOnPlayer = box(rock.pos.x, rock.pos.y, rock.size)
             .isColliding.rect.cyan;
 
         if(isOnPlayer && rock.throwCooldownCount == 0)
@@ -220,8 +243,10 @@ function ThrowInput()
 
         player.selected.velocity = vec(player.velocity.x * player.throwSpeed,
             player.velocity.y * player.throwSpeed);
+
+        player.selected.enableGravity = true;
+
         player.selected = null;
-        player.selected.Enablegravity == true;
     }
 }
 
